2b3cd4629a4d22b94f7b2698f9d81d2d0ae9ecbd
alexandre.borgoltz <alexandre.borgoltz@gmail.com>
2011-06-17 09:46:59 +0000

    This is the first version! Unit test may come soon...

---
+++ com4e.core/src/com4e/core/api/IComObject.java
@@ -0,0 +1,165 @@
+package com4e.core.api;
+
+public interface IComObject {
+	int getAddress();
+
+	int method0(int[] args);
+
+	int method1(int[] args);
+
+	int method2(int[] args);
+
+	int method3(int[] args);
+
+	int method4(int[] args);
+
+	int method5(int[] args);
+
+	int method6(int[] args);
+
+	int method7(int[] args);
+
+	int method8(int[] args);
+
+	int method9(int[] args);
+
+	int method10(int[] args);
+
+	int method11(int[] args);
+
+	int method12(int[] args);
+
+	int method13(int[] args);
+
+	int method14(int[] args);
+
+	int method15(int[] args);
+
+	int method16(int[] args);
+
+	int method17(int[] args);
+
+	int method18(int[] args);
+
+	int method19(int[] args);
+
+	int method20(int[] args);
+
+	int method21(int[] args);
+
+	int method22(int[] args);
+
+	int method23(int[] args);
+
+	int method24(int[] args);
+
+	int method25(int[] args);
+
+	int method26(int[] args);
+
+	int method27(int[] args);
+
+	int method28(int[] args);
+
+	int method29(int[] args);
+
+	int method30(int[] args);
+
+	int method31(int[] args);
+
+	int method32(int[] args);
+
+	int method33(int[] args);
+
+	int method34(int[] args);
+
+	int method35(int[] args);
+
+	int method36(int[] args);
+
+	int method37(int[] args);
+
+	int method38(int[] args);
+
+	int method39(int[] args);
+
+	int method40(int[] args);
+
+	int method41(int[] args);
+
+	int method42(int[] args);
+
+	int method43(int[] args);
+
+	int method44(int[] args);
+
+	int method45(int[] args);
+
+	int method46(int[] args);
+
+	int method47(int[] args);
+
+	int method48(int[] args);
+
+	int method49(int[] args);
+
+	int method50(int[] args);
+
+	int method51(int[] args);
+
+	int method52(int[] args);
+
+	int method53(int[] args);
+
+	int method54(int[] args);
+
+	int method55(int[] args);
+
+	int method56(int[] args);
+
+	int method57(int[] args);
+
+	int method58(int[] args);
+
+	int method59(int[] args);
+
+	int method60(int[] args);
+
+	int method61(int[] args);
+
+	int method62(int[] args);
+
+	int method63(int[] args);
+
+	int method64(int[] args);
+
+	int method65(int[] args);
+
+	int method66(int[] args);
+
+	int method67(int[] args);
+
+	int method68(int[] args);
+
+	int method69(int[] args);
+
+	int method70(int[] args);
+
+	int method71(int[] args);
+
+	int method72(int[] args);
+
+	int method73(int[] args);
+
+	int method74(int[] args);
+
+	int method75(int[] args);
+
+	int method76(int[] args);
+
+	int method77(int[] args);
+
+	int method78(int[] args);
+
+	int method79(int[] args);
+}
